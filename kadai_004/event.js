da212ea97cdd1087ac6dac573632506ee700d8ec
yashi-yashi09 <k.m.ak04129@gmail.com>
2024-08-18 23:17:28 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+$(window).on(
+        'load', () => {
+        console.log('loadイベントが発生しました');
+    });
+
+$(window).on(
+    'scroll', () => {
+        console.log('scrollイベントが発生しました');
+    });
+
+      
+        
